ec2b9225eb88f9c7667f5ce8c82cf390235a938c
yatniel <yatniel@qq.com>
2020-03-13 20:17:26 +0800

    基础map

--- model/index.go
+++ model/index.go
@@ -9,4 +9,15 @@ func init() {
 	ark.Register("test", Map{
 		"name": "asdf",
 	})
+
+	// ark.Register("int", ark.Type{
+	// 	Name: "整型", Desc: "整型",
+	// 	Valid: func() bool {
+	// 		return false
+	// 	},
+	// 	Value: func(val Any) Any {
+	// 		return val
+	// 	},
+	// })
+
 }
